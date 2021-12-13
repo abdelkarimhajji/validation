ce6f600b52a34d3c41a3875d99a537bf3eec1b65
karimhajji1 <hajji.karim.solicode@gmail.com>
2021-12-13 15:33:22 +0100

    finish

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+    
+
+var selectedRow = null;
 function validation(){
     var valid = true;
 
@@ -107,32 +110,128 @@ function validation(){
     var Bande3 = document.getElementById('Bande-Dessinée');
 
 
-       if(!(Roman1.checked||Essai2.checked||Bande3.checked)){
-        h4.innerHTML ="Le Type: *[Required]";
-        h4.style.color ="red"
+       if(Roman1.checked||Essai2.checked||Bande3.checked){
+        h4.innerHTML ="Le Type: *";
+        h4.style.color ="black"
         valid = false ;
+       }
+       else if(!(getSelectedByValue)){
+        h4.innerHTML ="Le Type: *";
+        h4.style.color ="red"
        }
        else{
         h4.innerHTML  = "Le Type *";
         h4.style.color ="black";
         valid = (valid) ? true : false ;
        }
+       var getSelectedByValue = document.querySelector(
+        'input[name="Roman"]:checked');
 
-        
+       var myTable = document.getElementById("table");
+       var row = myTable.insertRow(-1);
+       var cell1 = row.insertCell(0);
+       var cell2 = row.insertCell(1);
+       var cell3 = row.insertCell(2);
+       var cell4 = row.insertCell(3);
+       var cell5 = row.insertCell(4);
+       var cell6 = row.insertCell(5);
+       var cell7 = row.insertCell(6);
+       
 
-    
+       cell1.innerHTML =firstName;
+       cell2.innerHTML =lastName;
+       cell3.innerHTML =prix;
+       cell4.innerHTML =date;
+       cell5.innerHTML =myLangue.value;
+       cell6.innerHTML =getSelectedByValue.value;
+       cell7.innerHTML = '<input type="submit" value="Edit" onclick="editRow(this)" id="btn">' + '<input type="submit" onclick="deleteRow(this)"  value="Delete" id="btn">';
+      
 
-    
     return valid;
 
 }
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    table.deleteRow(i);
+}
+function editRow(td) {
+    var  type = document.getElementsByClassName("radio");
+   var selectedRow = td.parentElement.parentElement.rowIndex;
+    var row = table.rows[selectedRow];
+    if(td.value == "Edit"){
+    document.getElementById("th1").value = row.cells[0].innerHTML;
+    document.getElementById("th2").value = row.cells[1].innerHTML;
+    document.getElementById("th3").value = row.cells[2].innerHTML;
+    document.getElementById("th4").value = row.cells[3].innerHTML;
+    document.getElementById("langue").value = row.cells[4].innerHTML;
+
+    for(var i=0;i<type.length;i++){
+        if(row.cells[5].innerHTML==type[i].value){
+            type[i].checked = true;
+        }
+    }
+    td.value="Save"
+    document.getElementById("submit").setAttribute("disabled","true");
+}
+else{
+            row.cells[0].innerHTML = document.getElementById("th1").value;
+            row.cells[1].innerHTML = document.getElementById("th2").value;
+            row.cells[2].innerHTML =  document.getElementById("th3").value;
+            row.cells[3].innerHTML = document.getElementById("th4").value;
+            row.cells[4].innerHTML = document.getElementById("langue").value;
+            for(var i=0;i<type.length;i++){
+                if(type[i].checked){
+                    row.cells[5].innerHTML = type[i].value;
+                }
+            }
+            td.value = "Edit";
+            document.getElementById("submit").removeAttribute("disabled")
+}
+    
+    }
+
+
+
+
     
 
+    
+    
 
+    function edit_row(r){
+        var i = r.parentNode.parentNode.rowIndex;
+        var row = table.rows[i];
+        if(r.value == "Edit"){
+            title.value = row.cells[0].innerHTML;
+            author.value = row.cells[1].innerHTML;
+            price.value = row.cells[2].innerHTML;
+            pub.value = row.cells[3].innerHTML;
+            lang.value = row.cells[4].innerHTML;
 
-
-
-
+            // Type
+            for(var i=0;i<type.length;i++){
+                if(row.cells[5].innerHTML==type[i].value){
+                    type[i].checked = true;
+                }
+            }
+            r.value="Save"
+            document.getElementById("submit_btn").setAttribute("disabled","true");
+        }
+        else{
+            row.cells[0].innerHTML = title.value;
+            row.cells[1].innerHTML = author.value;
+            row.cells[2].innerHTML = price.value;
+            row.cells[3].innerHTML = pub.value;
+            row.cells[4].innerHTML = lang.options[lang.selectedIndex].value;
+            for(var i=0;i<type.length;i++){
+                if(type[i].checked){
+                    row.cells[5] = type[i].value;
+                }
+            }
+            r.value = "Edit";
+            document.getElementById("submit_btn").removeAttribute("disabled")
+        }
+    }
 
 
 
@@ -216,4 +315,4 @@ function validation(){
 
 
 
-// JavaScript Array Map
+/*JavaScript Array Map*/
